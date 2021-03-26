1a03e0e6a3665d6cf2db52d55bdb6560812380b5
ssamae <ssamae87@gmail.com>
2021-03-27 00:35:34 +0530

    2-20

--- controllers/userController.js
+++ controllers/userController.js
@@ -4,5 +4,9 @@ export const logout = (req,res) => res.render("logout",{pageTitle:"Logout"});
 
 export const users =(req,res) => res.render("users", {pageTitle:"Users"});
 export const userDetail =(req,res) => res.render("userDetail", {pageTitle:"User Detail"});
-export const editProfile =(req,res) => res.render("editProfile",{pageTitle:"Edit Profile"});
+
+export const editProfile =(req,res) => {
+    res.render("editProfile",{pageTitle:"Edit Profile"});
+    }
+
 export const changePassword =(req,res) => res.render("changePassword",{pageTitle:"Change Password"});
